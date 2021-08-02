3c1d3739ed791dbd98460e67ab4a14d7c4882316
alexbraatz <alexjbraatz@gmail.com>
2021-08-02 16:05:04 -0500

    working on making map mobile friendly

--- components/Contact.js
+++ components/Contact.js
@@ -5,6 +5,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 import { useState, useEffect } from 'react'
 
+import { isMobile } from 'react-device-detect';
+
 const mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
 
 export const Contact = () => {
@@ -35,6 +37,19 @@ export const Contact = () => {
       );
   }, [])
 
+  const styles = {
+    height: 400, 
+    width: 400
+  }
+
+  const mapSize = () => {
+    if ( isMobile ){
+      styles = { 
+        height: 50, 
+        width: 50}
+    }
+  }
+
   return (
 
     <section className="pt-24 w-screen h-auto bg-gradient-to-r from-pink-600 to-purple-500">
@@ -49,14 +64,14 @@ export const Contact = () => {
       <h6 className="pl-9 pt-2 font-sans font-extrabold">currently located in:</h6>
       <div className="px-40 py-8 grid grid-rows-3 grid-flow-col place-content-start"> 
         
-        <div className="ml-2 shadow-2xl sm:w-5 rounded-lg col-span-1 row-span-3" id='my-map' style={{ height: 400, width: 400 }}></div>
+        <div className="ml-2 shadow-2xl sm:w-5 rounded-lg col-span-1 row-span-3" id='my-map' style={styles}></div>
 
         <div className="pl-24 ml-8">
           <a href="https://github.com/alexbraatz" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faGithub} style={{width: '120px'}}/>
           </a>
         </div>
-        
+
         <div className="pl-24 ml-8">
           {/* target attr opens a new tab and rel attr prevents a phishing type known as tabnabbing */}
           <a href="https://www.linkedin.com/in/alexjbraatz/" target="_blank" rel="noopener noreferrer">
