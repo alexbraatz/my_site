b827b1cb3caf14ddf3b7e2d10119a68a78131a29
alexbraatz <alexjbraatz@gmail.com>
2021-08-04 13:59:37 -0500

    formatting who component to mobile

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 export const Footer = () => {
 
   return (
-    <section className="pt-24 w-full bg-gradient-to-r from-pink-600 to-purple-500">
+    <section className="pt-24 w-auto h-auto bg-gradient-to-r from-pink-600 to-purple-500">
       <h6 className="grid justify-items-center font-bold">&copy; 2021 Alex Braatz</h6>
     </section>
   )
--- components/Who.js
+++ components/Who.js
@@ -2,32 +2,33 @@ export const Who = () => {
 
   return (
     <section className="pt-24 w-full h-screen bg-gradient-to-r from-pink-600 to-purple-500">
-      <div className="grid grid-rows-3 grid-flow-col gap-4">
-        <div className="row-span-3">
-          <h2 className="pl-7 font-sans font-extrabold text-4xl md:text-7xl ubpixel-antialiased text-black">me, myself, and i</h2>
+      {/* <div className="relative h-32 w-32"> */}
+        <div className="float-left">
+          <h2 className="decoration-clone pl-2 font-sans font-extrabold text-4xl ubpixel-antialiased text-black">
+            me,<br/>myself,<br/>and i
+            </h2>
         </div>
-        <div className="pr-2 col-span-2">
-          <p>i'm alex braatz, your friendly neighborhood developer. from my first 'hello world' in python in spring of 2020, 
+        <div className="">
+          <p className="">i'm alex braatz, your friendly neighborhood developer. from my first 'hello world' in python in spring of 2020, 
              it was like being struck by lightning. my passion has always been to help people. 
              now, i can build the tools of tomorrow to impact, improve, and help the lives of as many people as i can.
           </p>
         </div>
-        <div className="pr-2 row-span-2 col-span-2">
-          <p>since then, i began my path to coding mastery by learning 'Ruby the Hard Way'. after learning some time on my own,
+        <div className="">
+          <p className="">since then, i began my path to coding mastery by learning 'Ruby the Hard Way'. after learning some time on my own,
              i attended a full stack immersive experience at Prime Digital Academy gaining a solid understanding of 
              modern web architect and accelerating my technical skills. now, i have proficieny in: 
              javascript, react, node.js, and postgresql. additionally, with self-taught experience in ruby, python, php,
              and contemporary frameworks like next.js
           </p>
         </div>
-      </div>
-
-      <div>
-        <p className="pl-2">i currently reside in minneapolis, minnesota with my partner samantha, 
-          husky puppy zelda, our bengal-mix kitty nooriya, and gray tabby-cat nala. in my free time i like spending time outside with
-          our dog, going on hikes (huge fan of yellowstone), video games, anime, netflix and of course, coding
-        </p>
-      </div>
+        <div className="">
+          <p className="">i currently reside in minneapolis, minnesota with my partner samantha, 
+            husky puppy zelda, our bengal-mix kitty nooriya, and gray tabby-cat nala. in my free time i like spending time outside with
+            our dog, going on hikes (huge fan of yellowstone), video games, anime, netflix and of course, coding
+          </p>
+        </div>
+      {/* </div> */}
     </section>
   )
 }
